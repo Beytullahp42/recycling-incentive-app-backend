c6e58b17a834b124296a24e5b237d574ba74e17a
Beytullahp42 <beytullahpay42@gmail.com>
2025-12-30 02:08:33 +0300

    cron

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:close-expired-sessions')->everyFiveMinutes();
+Schedule::command('app:rotate-seasons')->hourly();
